cdcbdb09a9b9a35285d809bd8f0e9212e4280ca7
Hussien Ele <58689416+Hussien-Elepiary@users.noreply.github.com>
2023-04-14 09:32:35 +0200

    Examples
    SetOprators
    AggregateOperators02
    PartitioningOperators
    Quantifiers
    Grouping

--- Linq Assignment 01 sln/Linq Assignment 01/Program.cs
+++ Linq Assignment 01 sln/Linq Assignment 01/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Threading;
 using System.IO;
 using static Linq_Assignment_01.ListGenerator;
+using System.Diagnostics.Metrics;
+using System.Xml;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Collections.Concurrent;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Linq_Assignment_01
 {
@@ -11,14 +21,15 @@ namespace Linq_Assignment_01
     {
         static void Main(string[] args)
         {
-            OrderingOperators();
+            Grouping();
         }
+        #region Linq_Assignment_01
 
         static void RestrictionOperators()
         {
             var result = ProductList.Where(p => p.UnitsInStock == 0);
 
-            result = ProductList.Where(p =>p.UnitsInStock == 0 && p.UnitPrice > 3.00M);
+            result = ProductList.Where(p => p.UnitsInStock == 0 && p.UnitPrice > 3.00M);
 
             foreach (Product item in result)
             {
@@ -32,14 +43,14 @@ namespace Linq_Assignment_01
 
             result = ProductList.FirstOrDefault(result => result.UnitPrice > 1000);
 
-            Console.WriteLine(result?.ProductName??"Null");
+            Console.WriteLine(result?.ProductName ?? "Null");
 
             int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
             var result01 = Arr.Where(a => a > 5).ToArray();
             Array.Sort(result01);
             Console.WriteLine(result01[1]);
         }
-        static void AggregateOperators() 
+        static void AggregateOperators()
         {
             ///1. Uses Count to get the number of odd numbers in the array
             //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
@@ -220,6 +231,279 @@ namespace Linq_Assignment_01
             //}
         }
 
+        #endregion
 
+        #region Linq_Assignment_02
+
+        public static string getLastCharacters(string text, int charactersCount)
+        {
+            int length = text.Length;
+            int offset = Math.Max(0, length - charactersCount);
+            return text.Substring(offset);
+        }
+
+        static void SetOprators()
+        {
+            //1.Find the unique Category names from Product List
+            var Catagories = ProductList.Select(P => P.Category).Distinct().ToList();
+
+            //2.Produce a Sequence containing the unique first letter from both product and customer names.
+            var UniqueLetter = ProductList.Select(P => P.ProductName)
+                                          .Concat(
+                                             CustomerList.Select(C => C.CustomerName)
+                                           ).Select(S => S[0]).ToArray().Distinct();
+            //3.Create one sequence that contains the common first letter from both product and customer names.
+
+            //4.Create one sequence that contains the first letters of product names that are not also first letters of customer names.
+
+            //5.Create one sequence that contains the last Three Characters in each name of all customers and products, including any duplicates
+            var LastThreeLetters = ProductList.Select(P => P.ProductName)
+                                              .Concat(
+                                                CustomerList.Select(C => C.CustomerName)
+                                                ).Select(S => getLastCharacters(S, 3));
+
+            foreach (var item in LastThreeLetters)
+            {
+                Console.WriteLine(item);
+            }
+        } 
+
+        static void AggregateOperators02()
+        {
+            //1.Get the total units in stock for each product category.
+            ////var TotalPricePerCat = ProductList.Where(P=>P.UnitsInStock>0).GroupBy(P => P.Category);
+
+            ////foreach (var item in TotalPricePerCat)
+            ////{
+            ////    Console.WriteLine(item.Key);
+            ////    foreach (var cat in item)
+            ////    {
+            ////        Console.WriteLine($"---------------------{cat.ProductName}");
+            ////    }
+            ////    Console.WriteLine("=========================================");
+            ////}
+
+            //2.Get the cheapest price among each category's products
+
+            ////var cheapestPriceAmongEachCategory = ProductList.MinBy(P => P.UnitPrice);
+            ////Console.WriteLine(cheapestPriceAmongEachCategory);
+
+            //3.Get the products with the cheapest price in each category(Use Let)
+
+            ////var cheapestPriceInEachCategoryUseLet = from P in ProductList
+            ////                                        group P by P.Category 
+            ////                                        into GP
+            ////                                        select GP.MinBy(P=>P.UnitPrice);
+
+            ////cheapestPriceInEachCategoryUseLet = ProductList.GroupBy(p => p.Category)
+            ////                                    .Select(GP=>GP.MinBy(P => P.UnitPrice)).ToArray();
+
+            ////foreach (var item in cheapestPriceInEachCategoryUseLet)
+            ////{
+            ////    Console.WriteLine($"Cat: {item.Category} || ProductName: {item.ProductName} || Price: {item.UnitPrice}");
+            ////}
+
+            //4.Get the most expensive price among each category's products.
+
+            ////var expensivePriceAmongEachCategory = ProductList.MaxBy(P => P.UnitPrice);
+            ////Console.WriteLine(expensivePriceAmongEachCategory);
+
+            //5.Get the products with the most expensive price in each category.
+
+            ////var expensivePriceInEachCategoryUseLet = from P in ProductList
+            ////                                        group P by P.Category
+            ////                                        into GP
+            ////                                        select GP.MaxBy(P => P.UnitPrice);
+
+            ////expensivePriceInEachCategoryUseLet = ProductList.GroupBy(p => p.Category)
+            ////                                    .Select(GP => GP.MaxBy(P => P.UnitPrice)).ToArray();
+
+            ////foreach (var item in expensivePriceInEachCategoryUseLet)
+            ////{
+            ////    Console.WriteLine($"Cat: {item.Category} || ProductName: {item.ProductName} || Price: {item.UnitPrice}");
+            ////}
+
+            //6.Get the average price of each category's products.
+
+            ////var averagePriceInEachCategoryUseLet = from P in ProductList
+            ////                                         group P by P.Category
+            ////                                        into GP
+            ////                                         select GP.Average(P => P.UnitPrice);
+
+            ////averagePriceInEachCategoryUseLet = ProductList.GroupBy(p => p.Category)
+            ////                                    .Select(GP => GP.Average(P => P.UnitPrice)).ToArray();
+
+            ////foreach (var item in averagePriceInEachCategoryUseLet)
+            ////{
+            ////    Console.WriteLine($"{item}");
+            ////}
+
+
+        }
+
+        static void PartitioningOperators()
+        {
+            //1.Get the first 3 orders from customers in Washington
+            ////var First3Orders = CustomerList.Where(C=>C.Country == "USA").Select(C=>new { C.CustomerName,Orders = C.Orders.Take(3) });
+
+            ////foreach (var item in First3Orders)
+            ////{
+            ////    Console.WriteLine(item.CustomerName);
+            ////    foreach (var order in item.Orders)
+            ////    {
+            ////        Console.WriteLine(order);
+            ////    }
+            ////}
+
+            //2.Get all but the first 2 orders from customers in Washington.
+
+            ////var AllButFirst2Orders = CustomerList.Where(C => C.Country == "USA").Select(C => new { C.CustomerName, Orders = C.Orders.Skip(2)});
+
+            ////foreach (var item in AllButFirst2Orders)
+            ////{
+            ////    Console.WriteLine(item.CustomerName);
+            ////    foreach (var order in item.Orders)
+            ////    {
+            ////        Console.WriteLine(order);
+            ////    }
+            ////}
+
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            //3.Return elements starting from the beginning of the array until a number is hit that is less than its position in the array.
+
+            ////var result01 = numbers.TakeWhile((number,index) => number > index);
+
+            ////foreach (var number in result01)
+            ////{
+            ////    Console.WriteLine(number);
+            ////}
+
+            //4.Get the elements of the array starting from the first element divisible by 3.
+
+            ////var result02 = numbers.TakeWhile(number=> number%3 == 0);
+
+            ////foreach (var number in result02)
+            ////{
+            ////    Console.WriteLine(number);
+            ////}
+
+
+            //5.Get the elements of the array starting from the first element less than its position.
+
+            ///Same as number 3 !!!
+        }
+
+        static void Qubtifiers()
+        {
+            //1.Determine if any of the words in dictionary_english.txt(Read dictionary_english.txt
+            //into Array of String First) contain the substring 'ei'.
+
+            //var text = from line in File.ReadAllLines("dictionary_english.txt")
+            //           where line.Contains("ei")
+            //           select line;
+
+            //foreach (var item in text)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //2.Return a grouped a list of products only for categories that have at least one product that is out of stock.
+
+            //var GroupOfOutOfStockCategory = ProductList.GroupBy(P => P.Category).Where(Gp => Gp.Any(ProductInGP => ProductInGP.UnitsInStock == 0));
+
+            //foreach (var group in GroupOfOutOfStockCategory)
+            //{ 
+            //    Console.WriteLine(group.Key);
+            //}
+
+            //3.Return a grouped a list of products only for categories that have all of their products in stock.
+
+
+            //var GroupOfInStockCategory = ProductList.GroupBy(P => P.Category).Where(Gp => Gp.All(ProductInGP => ProductInGP.UnitsInStock != 0));
+
+            //foreach (var group in GroupOfInStockCategory)
+            //{
+            //    Console.WriteLine(group.Key);
+            //}
+
+        }
+
+        static void Grouping()
+        {
+            //1.Use group by to partition a list of numbers by their remainder when divided by 5
+            ////List<int> numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            ////var DevidedBy5Group = numbers.GroupBy(Number=> Number%5);
+
+            ////foreach (var item in DevidedBy5Group)
+            ////{
+            ////    Console.Write($"Numbers with a remainder of {item.Key} when devided by 5:");
+            ////    foreach (var number in item)
+            ////    {
+            ////        Console.Write($" {number}");
+            ////    }
+            ////    Console.WriteLine();
+            ////}
+
+            //2.Uses group by to partition a list of words by their first letter. Use dictionary_english.txt for Input
+            ////var GroupedTextByFirstLetter = File.ReadAllLines("dictionary_english.txt").GroupBy(line => line[0]);
+
+            ////foreach (var item in GroupedTextByFirstLetter)
+            ////{
+            ////    Console.Write($"lines start With the Letter: {item.Key}");
+            ////    foreach (var line in item)
+            ////    {
+            ////        Console.Write($" {line}");
+            ////    }
+            ////    Console.WriteLine();
+            ////}
+
+            //3.Consider this Array as an Input
+            string[] Arr = { "from", "salt", "earn", "last", "near", "form"};
+
+            // First Triming Spaces an converting the all Chars to Lower Case in case of any upper case deffrancse 
+            //then makes a new opject of ArrEqualityComparer to start Grouping passed on the character Sorting like (from and form) 
+            //var orderGroups = Arr.GroupBy(
+            //        w => w.Trim(),
+            //        a => a.ToLower(),
+            //        new ArrEqualityComparer()
+            //        );
+
+            //foreach (var v in orderGroups)
+            //{
+            //    Console.WriteLine(v.Key);
+            //    foreach (var g in v)
+            //    {
+            //        Console.WriteLine(g);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+        }
+
+        public class ArrEqualityComparer : IEqualityComparer<string>
+        {
+            public bool Equals(string x, string y)
+            {
+                // Takes two Values and Compare the result of getCanonicalString()
+                return getCanonicalString(x) == getCanonicalString(y);
+            }
+
+            public int GetHashCode(string obj)
+            {
+                return getCanonicalString(obj).GetHashCode();
+            }
+
+            private string getCanonicalString(string word)
+            {
+                // Sorts the Characters of the Word like ABC... 
+                // then return the String
+                char[] wordChars = word.ToCharArray();
+                Array.Sort<char>(wordChars);
+                return new string(wordChars);
+            }
+        }
+        #endregion
     }
 }
